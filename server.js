593a1139a76b44910dd93a2304ccec0d2a414348
sigert goeminne <sigertgoeminne@gmail.com>
2016-03-19 15:43:13 +0100

    f

--- server.js
+++ server.js
@@ -5,13 +5,65 @@
  */
 
 
-var app = require('http').createServer(handler)
+var app = require('http').createServer(requestHandler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 var port = process.env.port || 1337;
+var path = require('path');
+
+
 
 app.listen(port);
 
+
+//helper function handles file verification
+function getFile(filePath,res,page404){
+	//does the requested file exist?
+	fs.exists(filePath,function(exists){
+		//if it does...
+		if(exists){
+			//read the fiule, run the anonymous function
+			fs.readFile(filePath,function(err,contents){
+				if(!err){
+					//if there was no error
+					//send the contents with the default 200/ok header
+					res.end(contents);
+				} else {
+					//for our own troubleshooting
+					console.dir(err);
+				};
+			});
+		} else {
+			//if the requested file was not found
+			//serve-up our custom 404 page
+			fs.readFile(page404,function(err,contents){
+				//if there was no error
+				if(!err){
+					//send the contents with a 404/not found header 
+					res.writeHead(404, {'Content-Type': 'text/html'});
+					res.end(contents);
+				} else {
+					//for our own troubleshooting
+					console.dir(err);
+				};
+			});
+		};
+	});
+};
+ 
+//a helper function to handle HTTP requests
+function requestHandler(req, res) {
+	var
+	fileName = path.basename(req.url) || 'index.html',
+	localFolder = __dirname ,
+	page404 = localFolder + '404.html';
+ 
+	//call our helper function
+	//pass in the path to the file we want,
+	//the response object, and the 404 page path
+	//in case the requestd file is not found
+	getFile((localFolder + fileName),res,page404);
+};
 	
 function handler(req, res) {
 	
