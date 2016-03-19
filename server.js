1fa443bc9c3b0cf0f04eff653bf8fe49b37483a4
sigert goeminne <sigertgoeminne@gmail.com>
2016-03-19 16:14:37 +0100

    f

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  */
 
 
-var app = require('http').createServer(handle);
+var app = require('http').createServer(requestHandler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 var port = process.env.port || 1337;
@@ -15,46 +15,25 @@ var path = require('path');
 
 app.listen(port);
 
-function handle (request, response) {
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
 
-  var contentTypesByExtension = {
-    '.html': "text/html",
-    '.css':  "text/css",
-    '.js':   "text/javascript"
-  };
-
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      var headers = {};
-      var contentType = contentTypesByExtension[path.extname(filename)];
-      if (contentType) headers["Content-Type"] = contentType;
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}
-
-/*
+ 
+//a helper function to handle HTTP requests
+function requestHandler(req, res) {
+	var
+	fileName = path.basename(req.url) || 'index.html';
+ 
+		fs.readFile(filePath,function(err,contents){
+				if(!err){
+					//if there was no error
+					//send the contents with the default 200/ok header
+					res.end(fileName);
+				} else {
+					//for our own troubleshooting
+					console.dir(err);
+				};
+			});
+};
 	
 
 io.on('connection', function (socket) {
@@ -63,4 +42,3 @@ io.on('connection', function (socket) {
 	});
 });
 
-*/
