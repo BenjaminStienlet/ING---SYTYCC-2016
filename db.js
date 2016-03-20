901b6d55f1f909f0d1616dbac5779d80499eb47f
Tom Stappaerts <tom.stappaerts@student.kuleuven.be>
2016-03-20 05:48:54 +0100

    fix

--- db.js
+++ db.js
@@ -45,7 +45,8 @@ module.exports =  {
     increaseUserAmount : increaseUserAmount,
     getHistoryForUser : getHistoryForUser,
     getNewsfeed : getNewsfeed,
-    getStocksForUser : getStocksForUser
+    getStocksForUser : getStocksForUser,
+    getStocksForUserCB : getStocksForUserCB
 };
 
 
@@ -260,8 +261,9 @@ function getStockPrice(stockId, callback) {
     var connection = new Connection(config);
     connection.on("connect", function(err) {
         request = new Request('SELECT price FROM stock WHERE sid = @stockId', function(err, rc, rows) {
-            if (err) {
+            if (err || !rows) {
                 console.log(err);
+                console.log(rows);
             }
             callback(columnsToJson(rows[0]));
             connection.close();
@@ -285,6 +287,7 @@ function sellStock(userId, stockId, timestamp, price, amount, profit) {
 
 function insertHistory(userId, stockId, timestamp, price, amount) {
     var connection = new Connection(config);
+    console.log("insertHistory",price);
     connection.on('connect',function(err) {
         request = new Request('INSERT INTO history (uid,sid,timestamp,price,amount)' +
                             'VALUES (@userId,@stockId,@timestamp,@price,@amount)', function(err) {
@@ -396,8 +399,9 @@ function getStocksForUserCB(userId, callback) {
     var connection = new Connection(config);
     connection.on('connect', function(err) {
         request = new Request('SELECT * FROM owenedshares WHERE uid = @userId', function(err, rc, rows) {
-            if (err) {
+            if (err || !rows) {
                 console.log(err);
+                console.log(userId);
             }
             callback(rowsToJson(rows));
             connection.close();
